62cb9c6d34ad309d538b13906b1397ca74b65f0b
Md Samsuzzoha Shayon <mdshayon0@gmail.com>
2020-11-16 15:52:05 +0600

    refactor booking and optamize little bit

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 const Event = require("../../models/Event");
 const User = require('../../models/User');
 const Booking = require('../../models/Booking');
-const {dateToString} = require('../../helper/date');
+const { dateToString } = require('../../helper/date');
 
 
 
@@ -23,6 +23,19 @@ const transformEvent = event => {
         date: dateToString(event._doc.date),
         // REPLACE USER 
         creator: user.bind(this, event.creator)
+    }
+}
+
+
+
+const transformBooking = booking => {
+    return {
+        ...booking._doc,
+        _id: booking.id,
+        user: user.bind(this, booking._doc.user),
+        event: singleEvent.bind(this, booking._doc.event),
+        createdAt: dateToString(booking._doc.createdAt),
+        updatedAt: dateToString(booking._doc.updatedAt)
     }
 }
 
@@ -123,14 +136,7 @@ module.exports = {
         try {
             const bookings = await Booking.find();
             return bookings.map(booking => {
-                return {
-                    ...booking._doc,
-                    _id: booking.id,
-                    user: user.bind(this, booking._doc.user),
-                    event: singleEvent.bind(this, booking._doc.event),
-                    createdAt: dateToString(booking._doc.createdAt),
-                    updatedAt: dateToString(booking._doc.updatedAt)
-                }
+                return transformBooking(booking)
             })
         } catch (err) {
             throw err;
@@ -252,14 +258,7 @@ module.exports = {
             event: fetchedEvent
         });
         const result = await booking.save();
-        return {
-            ...result._doc,
-            _id: result.id,
-            user: user.bind(this, booking._doc.user),
-            event: singleEvent.bind(this, booking._doc.event),
-            createdAt: dateToString(result._doc.createdAt),
-            updatedAt: dateToString(result._doc.updatedAt),
-        }
+        return transformBooking(result);
     },
 
 
