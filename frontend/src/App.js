4cb3da65a9201e11d8f8940db975ca8776de10ab
Md Samsuzzoha Shayon <mdshayon0@gmail.com>
2020-11-16 20:35:47 +0600

    react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import Auth from "./pages/Auth";
+import Events from "./pages/Events";
+import Bookings from "./pages/Bookings";
 
 function App() {
   return (
-    <div className="App">
-      <h1> graphql</h1>
-      <p>Hello</p>
-    </div>
+    <BrowserRouter >
+      <Switch>
+        <Redirect from="/" to="/auth" exact />
+        {/* <Route path="/" component={null} /> */}
+        <Route path="/auth" component={Auth} />
+        <Route path="/events" component={Events} />
+        <Route path="/bookings" component={Bookings} />
+      </Switch>
+
+    </BrowserRouter>
+
   );
 }
 
---
+++ frontend/src/pages/Auth.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Auth extends Component{
+    render(){
+        return (
+            <div className="container">Authpage</div>
+        );
+    }
+}
+
+
+export default Auth;
