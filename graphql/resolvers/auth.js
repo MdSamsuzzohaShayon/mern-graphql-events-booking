af061772357414319263b6a57969e7868bcb9f2a
Md Samsuzzoha Shayon <mdshayon0@gmail.com>
2020-11-16 18:36:20 +0600

    login route endpoint

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../../models/User');
 
 
@@ -36,5 +37,22 @@ module.exports = {
 
 
 
-
+    login: async ({ email, password }) => {
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            throw new Error("User does not exist");
+        }
+        const isEqual = await bcrypt.compare(password, user.password);
+        if (!isEqual) {
+            throw new Error("Password incorrect");
+        }
+        const token = jwt.sign({userId: (await user).id, email: user.email}, 'somesupersecretkey', {
+            expiresIn: '1h'
+        });
+        return {
+            userId: user.id,
+            token: token,
+            tokenExpiration: 1
+        }
+    }
 }
