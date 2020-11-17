7b773dab63aadaf75d7510c1d09c2cb15efd8b49
Md Samsuzzoha Shayon <mdshayon0@gmail.com>
2020-11-17 16:57:43 +0600

    using react ref and getting form value

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -1,29 +1,54 @@
 import React, { Component } from 'react';
-import { Container, Form, Input, Button } from 'semantic-ui-react';
+import { Container, Form, Button } from 'semantic-ui-react';
 
 class Auth extends Component {
+    // REFERANCES OR REF API BUILT INTO REACT 
+    // https://reactjs.org/docs/refs-and-the-dom.html
+    constructor(props) {
+        super(props);
+        this.emailEl = React.createRef();
+        this.passwordEl = React.createRef();
+
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault();
+        const email = this.emailEl.current.value;
+        const password = this.passwordEl.current.value;
+
+
+        if (email.trim().length === 0 || password.trim().length === 0) {
+            return;
+        }
+
+        console.log(email, password);
+
+
+
+
+
+
+    }
+
     render() {
         return (
             <Container>
-                <br/><br/>
-                <Form>
+                <br /><br />
+                <Form onSubmit={this.submitHandler}>
                     <Form.Group widths="equal">
-                        <Form.Field
-                            control={Input}
-                            label="Email"
-                            placeholder="Enter your email"
-                        />
-                        <Form.Field
-                            control={Input}
-                            label="Password"
-                            placeholder="Enter your password"
-                            type="password"
-                        />
+                        <Form.Field>
+                            <label className="ui header blue" >Email</label>
+                            <input type="email" ref={this.emailEl} placeholder='Enter your email' />
+                        </Form.Field>
+                        <Form.Field>
+                            <label className="ui header blue" >Password</label>
+                            <input type="password" ref={this.passwordEl} placeholder='Enter your password' />
+                        </Form.Field>
                     </Form.Group>
                     <Button.Group>
                         <Button color="teal" type="submit">Submit</Button>
                         <Button.Or />
-                        <Button color="teal" type="submit">Switch to Sign Up</Button>
+                        <Button color="teal" type="button">Switch to Sign Up</Button>
                     </Button.Group>
                 </Form>
             </Container>
